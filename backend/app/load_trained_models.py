23fc4667d48082f70f181b19ce2f976d69ac9e15
Sravana Neeli <sravana.neeli@gmail.com>
2023-06-04 16:15:36 -0700

    Folder restructure for the project

--- backend/app/load_trained_models.py
+++ backend/app/load_trained_models.py
@@ -65,6 +65,18 @@ if not os.path.exists(f'{PWD}/{MODELS_DIR}/imagenet1000_clsidx_to_labels.txt'):
 with open(f'{PWD}/{MODELS_DIR}/imagenet1000_clsidx_to_labels.txt') as f:
     IMGNET_ID_TO_LABEL = eval(f.read())
 
+CARDS_CLASSES = ['ace of clubs', 'ace of diamonds', 'ace of hearts', 'ace of spades', 'eight of clubs',
+                 'eight of diamonds', 'eight of hearts', 'eight of spades', 'five of clubs',
+                 'five of diamonds', 'five of hearts', 'five of spades', 'four of clubs', 'four of diamonds', 
+                 'four of hearts', 'four of spades', 'jack of clubs', 'jack of diamonds', 'jack of hearts', 
+                 'jack of spades', 'joker', 'king of clubs', 'king of diamonds', 'king of hearts', 
+                 'king of spades', 'nine of clubs', 'nine of diamonds', 'nine of hearts', 'nine of spades', 
+                 'queen of clubs', 'queen of diamonds', 'queen of hearts', 'queen of spades', 'seven of clubs',
+                 'seven of diamonds', 'seven of hearts', 'seven of spades', 'six of clubs', 'six of diamonds', 
+                 'six of hearts', 'six of spades', 'ten of clubs', 'ten of diamonds', 'ten of hearts', 
+                 'ten of spades', 'three of clubs', 'three of diamonds', 'three of hearts', 'three of spades', 
+                 'two of clubs', 'two of diamonds', 'two of hearts', 'two of spades']
+
 
 def load_models():
     """
@@ -75,19 +87,19 @@ def load_models():
         dict: loaded models
     """
 
-    resnet50 = tf.saved_model.load(f'{PWD}/{MODELS_DIR}/resnet-50-i224')
+    cards_model = tf.saved_model.load(f'{PWD}/{MODELS_DIR}/poker_cards_image_model/')
 
     models = {
-        'resnet50': resnet50.signatures['serving_default']
+        'cards_model': cards_model.signatures['serving_default']
     }
 
     return models
 
-download_ckpts()
-logging.info(msg="Checkpoints downloaded")
+# download_ckpts()
+# logging.info(msg="Checkpoints downloaded")
 
-convert_ckpts()
-logging.info(msg="Checkpoints converted")
+# convert_ckpts()
+# logging.info(msg="Checkpoints converted")
 
 MODELS_DICT = load_models()
 logging.info(msg="Models Loaded")
--- backend/app/routers/image_classification.py
+++ backend/app/routers/image_classification.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from app import oauth2
 from app.database import get_db
-from app.load_trained_models import IMGNET_ID_TO_LABEL, MODELS_DICT
+from app.load_trained_models import IMGNET_ID_TO_LABEL, MODELS_DICT, CARDS_CLASSES
 
 router = APIRouter(
     prefix="/image_classification",
@@ -12,7 +12,7 @@ router = APIRouter(
 )
 
 
-@router.post("/predict")
+@router.post("/cards_image/predict")
 async def classify_image(db: Session = Depends(get_db),
                          file: UploadFile = File(...),
                          get_current_user: int = Depends(oauth2.get_current_user)):
@@ -31,10 +31,10 @@ async def classify_image(db: Session = Depends(get_db),
     if extension in ("jpg", "jpeg"):
         img = tf.io.decode_jpeg(await file.read())
     else:
-        img = tf.io.decode_png(await file.read())
+        img = tf.io.decode_png(await file.read(), channels=3)
     img = tf.expand_dims(img, axis=0)
-    prediction = MODELS_DICT['resnet50'](img)
+    prediction = MODELS_DICT['cards_model'](img)
     id = int(tf.argmax(prediction['probs'][0]).numpy())
     return {
-        "class": IMGNET_ID_TO_LABEL[id]
+        "class": CARDS_CLASSES[id]
     }
